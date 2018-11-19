93e5b1cd16395bba583cf73e95cd8bb0c94d795e
Matiss Janis Aboltins <matiss@mja.lv>
2018-11-19 15:17:13 +0200

    Attempt to catch issues

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const yahooFinance = require('yahoo-finance');
 const ynab = require('ynab');
 
 module.exports.run = async (event, context, callback) => {
+  try {
   const ynabAPI = new ynab.API(process.env.YNAB_API_TOKEN);
   const automaticApproval = process.env.AUTOMATIC_APPROVAL === true || process.env.AUTOMATIC_APPROVAL === 'true';
   const transactions = [];
@@ -64,4 +65,7 @@ module.exports.run = async (event, context, callback) => {
   }
 
   callback(null, `Created ${transactions.length} transactions.`);
+} catch (e) {
+  callback(e);
+}
 }
